6182bb516f82e5fda1279d4162d9d7bf01ff4538
Nemanja <nradovanovic12022rn@raf.rs>
2025-07-23 00:43:11 +0200

    fix: null exceptions

--- Bank.UserService/Mappers/TransactionMapper.cs
+++ Bank.UserService/Mappers/TransactionMapper.cs
@@ -13,10 +13,10 @@ public static class TransactionMapper
         return new TransactionResponse
                {
                    Id              = transaction.Id,
-                   FromAccount     = transaction.FromAccount!.ToSimpleResponse(),
-                   FromCurrency    = transaction.FromCurrency!.ToResponse(),
-                   ToAccount       = transaction.ToAccount!.ToSimpleResponse(),
-                   ToCurrency      = transaction.ToCurrency!.ToResponse(),
+                   FromAccount     = transaction.FromAccount?.ToSimpleResponse()!,
+                   FromCurrency    = transaction.FromCurrency?.ToResponse()!,
+                   ToAccount       = transaction.ToAccount?.ToSimpleResponse()!,
+                   ToCurrency      = transaction.ToCurrency?.ToResponse()!,
                    FromAmount      = transaction.FromAmount,
                    ToAmount        = transaction.ToAmount,
                    Code            = transaction.Code?.ToResponse()!,
